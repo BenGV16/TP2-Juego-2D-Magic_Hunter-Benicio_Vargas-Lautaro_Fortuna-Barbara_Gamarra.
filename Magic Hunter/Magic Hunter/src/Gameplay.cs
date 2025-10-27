5ebc4ecfed4ab91d1d98959f6e18db84e9fae865
benicio vargas <beniciovargas161207@gmail.com>
2025-10-27 17:08:22 -0300

    solucion de error de multiples eliminaciones

--- Magic Hunter/Magic Hunter/src/Gameplay.cs
+++ Magic Hunter/Magic Hunter/src/Gameplay.cs
@@ -3,6 +3,8 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Magic_Hunter.src;
 
@@ -10,9 +12,11 @@ public class GamePlay
 {
     private Texture2D _pixel;
     private List<BoxEntity> _boxes = new();
+    private MouseState _previousMouseState;
     private double _spawnTimer = 0;
     private double _spawnInterval;
-    private const int MaxBoxes = 4;
+    float depth = 1.0f;
+    private const int MaxBoxes = 10;
     private Random _random = new();
 
     public void Initialize(GraphicsDevice graphicsDevice, Viewport viewport)
@@ -24,51 +28,61 @@ public class GamePlay
 
     public void Update(GameTime gameTime, Viewport viewport, MouseState mouseState)
     {
+        var ms = Mouse.GetState();
         _spawnInterval = _random.NextDouble() * 4.0 + 1.0;
         float delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
         foreach (var box in _boxes)
         {
             box.Update(gameTime, viewport);
         }
-        if (mouseState.LeftButton == ButtonState.Pressed)
+        if (mouseState.LeftButton == ButtonState.Pressed && _previousMouseState.LeftButton == ButtonState.Released)
         {
             Point mousePosition = new Point(mouseState.X, mouseState.Y);
-            for (int i = _boxes.Count - 1; i >= 0; i--)
+            var clickedBoxes = _boxes.Where(box => box.Rect.Contains(mousePosition)).ToList();
+
+            if (clickedBoxes.Count > 0)
             {
-                if (_boxes[i].Rect.Contains(mousePosition))
-                {
-                    _boxes.RemoveAt(i);
-                    break;
-                }
+                var frontBox = clickedBoxes.OrderBy(box => box.Depth).First();
+                _boxes.Remove(frontBox);
             }
         }
+
         _spawnTimer += gameTime.ElapsedGameTime.TotalSeconds;
         if (_spawnTimer >= _spawnInterval && _boxes.Count < MaxBoxes)
         {
             _spawnTimer = 0;
             AddBox(viewport);
         }
+        _previousMouseState = ms;
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
         foreach (var box in _boxes)
         {
-            spriteBatch.Draw(_pixel, box.Rect, Color.OrangeRed * 0.8f);
+            Color[] colors = { Color.Blue, Color.Green, Color.Purple };
+            Color col = colors[_random.Next(colors.Length)];
+            spriteBatch.Draw(_pixel, box.Rect, null, box.Color * 0.8f, 0f, Vector2.Zero, SpriteEffects.None, box.Depth);
         }
     }
 
     private void AddBox(Viewport viewport)
     {
+        Color[] colors = { Color.Blue, Color.Green, Color.Purple, Color.Yellow, Color.Red, Color.White };
+        Color col = colors[_random.Next(colors.Length)];
         int startX = _random.Next(0, viewport.Width - 40);
         Vector2 startPos = new Vector2(startX, viewport.Height / 2f);
-        _boxes.Add(new BoxEntity(startPos, _random));
+        _boxes.Add(new BoxEntity(startPos, _random, depth, col));
     }
     private class BoxEntity
     {
         public Rectangle Rect;
         private Vector2 _position;
         private Vector2 _targetPosition;
+        public Color Color { get; private set; }
+        private float _depth;
+        public bool IsMoving => (_targetPosition - _position).Length() > 1f;
+        public float Depth => _depth;
         private float _speed = 100f;
         private float _width = 40f;
         private float _height = 40f;
@@ -76,18 +90,19 @@ public class GamePlay
         private double _moveInterval;
         private Random _random;
 
-        public BoxEntity(Vector2 startPos, Random sharedRandom)
+        public BoxEntity(Vector2 startPos, Random sharedRandom, float depth, Color color)
         {
             _position = startPos;
             _targetPosition = startPos;
             _random = sharedRandom;
+            _depth = depth;
+            Color = color;
             _moveInterval = _random.NextDouble() * 4.0 + 1.0;
             UpdateRect();
         }
         public void Update(GameTime gameTime, Viewport viewport)
         {
             float delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
-
             Vector2 direction = _targetPosition - _position;
             if (direction.Length() > 1f)
             {
